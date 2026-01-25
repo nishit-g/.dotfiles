666d09082c748f1eca49816becd393210072c820
Nishit Gupta <nishit.gupta6708@gmail.com>
2026-01-25 11:07:44 +0530

    perf(nvim): add performance options (smoothscroll, splitkeep, etc.)
    - Add laststatus=3 (global statusline)
    - Add smoothscroll=true (Neovim 0.10+)
    - Add splitkeep=screen (stable content on splits)
    - Add inccommand=split (live substitution preview)
    - Add jumpoptions=stack (simpler jumplist)
    - Add diffopt algorithm:histogram, linematch:60, indent-heuristic
    - Add shada optimization for faster history
    - Add shortmess:append cIWa (less noise)
    - Add fillchars for cleaner UI
    - Disable swapfile (have undofile)

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -26,6 +26,34 @@ o.timeoutlen = 400
 o.wrap = false
 
 o.undofile = true
+o.swapfile = false  -- Disabled since we have undofile
+
+-- Performance
+o.laststatus = 3  -- Global statusline
+o.smoothscroll = true  -- Smooth scrolling (0.10+)
+o.splitkeep = "screen"  -- Keep content stable on splits
+o.inccommand = "split"  -- Live substitution preview
+o.jumpoptions = "stack"  -- Simpler jumplist
+
+-- Better diff
+o.diffopt:append("algorithm:histogram")
+o.diffopt:append("linematch:60")
+o.diffopt:append("indent-heuristic")
+
+-- Faster shada
+o.shada = "!,'1000,<50,s10,h"
+
+-- Less noise
+o.shortmess:append("cIWa")
+
+-- Cleaner UI
+o.fillchars = {
+  eob = " ",  -- Hide ~ at end of buffer
+  fold = " ",
+  foldopen = "v",
+  foldsep = "|",
+  diff = "/",
+}
 
 g.mapleader = " "
 g.maplocalleader = " "
