cf0c069e07940b247eaf79c26adfbc3ca0b399f3
Nishit Gupta <nishit.gupta6708@gmail.com>
2026-01-25 10:38:24 +0530

    perf(nvim): disable built-in plugins and optimize lazy.nvim
    - Disable 20+ unused built-in plugins (netrw, tar, zip, etc.) for -5-10ms
    - Add lazy.nvim performance config with disabled_plugins
    - Set defaults.lazy = true to prevent future eager loads
    - Disable unused providers (perl, ruby, python3)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,34 @@ if vim.loader then
 	vim.loader.enable()
 end
 
+-- Disable unused built-in plugins for faster startup
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_spec = 1
+vim.g.loaded_tutor_mode_plugin = 1
+
+-- Disable unused providers
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+-- vim.g.loaded_node_provider = 0  -- Keep if using copilot
+vim.g.loaded_python3_provider = 0
+
 require("options")
 require("keymaps")
 require("autocmds")
@@ -24,7 +52,24 @@ require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
 	},
+	defaults = {
+		lazy = true,  -- All plugins lazy by default
+	},
 	ui = {
 		border = "rounded",
+	},
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
 	},
 })
